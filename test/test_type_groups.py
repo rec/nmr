2bcf70aa536ba2116325fbda78d6b142038b7c82
Tom Ritchford <tom@swirly.com>
2022-12-19 15:50:09 +0100

    Add computations to type_groups and test

---
+++ test/test_type_groups.py
@@ -0,0 +1,45 @@
+from nmr.types import type_groups as tg
+import pytest
+
+ROUND_TRIP_CASES = [
+    (0, 0, tg.Math.integer),
+    (1, 0, tg.Science.element),
+    (2, 0, tg.Music.rhythm),
+    (7, 0, tg.Commercial.isbn),
+
+]
+
+NON_ROUND_TRIP_CASES = [
+    (8, 0, tg.Math.integer),
+]
+
+ALL_CASES = (
+    list(range(16))
+    + list(range(n := 109223, n + 32))
+    + list(range(n := 3458702354876246, n + 32))
+)
+
+
+def test_simple():
+    assert tg.number_to_remainder_and_type(8) == (0, tg.Math.integer)
+
+
+@pytest.mark.parametrize('n,d,t', ROUND_TRIP_CASES)
+def test_type_groups(n, d, t):
+    assert t.type_to_number(d) == n
+    assert tg.number_to_remainder_and_type(n) == (d, t)
+
+
+@pytest.mark.parametrize('n,d,t', NON_ROUND_TRIP_CASES)
+def test_type_groups_non(n, d, t):
+    assert (n2 := t.type_to_number(d)) != n
+    assert t.type_to_number(n2) == n2
+    assert tg.number_to_remainder_and_type(n) == (d, t)
+
+
+@pytest.mark.parametrize('n', ALL_CASES)
+def test_type_groups_all(n):
+    r, t = tg.number_to_remainder_and_type(n)
+    n2 = t.type_to_number(r)
+
+    assert tg.number_to_remainder_and_type(n2) == (r, t)
