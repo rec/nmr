e5d36610b7c6e469fb9d2c59b7ef938899c149ec
Tom Ritchford <tom@swirly.com>
2022-12-19 15:50:09 +0100

    Add computations to type_groups and test

--- nmr/types/type_groups.py
+++ nmr/types/type_groups.py
@@ -1,50 +1,91 @@
 from enum import IntEnum, auto
+from functools import lru_cache
 
 
-class Group(IntEnum):
+class Type:
+    @classmethod
+    @lru_cache()
+    def length(cls):
+        items = list(cls)
+        if items[-1].name == 'RADIX':
+            items.pop()
+        return len(items)
+
+    @classmethod
+    def divmod(cls, n: int, strict=False):
+        d, m = divmod(n, cls.RADIX)
+        if m >= cls.length():
+            if strict:
+                raise IndexError(f'Out of range {m=}, {cls.length()=}')
+            m = m % cls.length()
+
+        print('divmod', cls, n, d, m)
+        return d, cls(m + 1)
+
+    RADIX = 8
+
+    def type_to_number(self, n):
+        g = Group[self.__class__.__name__.lower()]
+        return g.value - 1 + Group.RADIX * (self.value - 1 + self.RADIX * n)
+
+
+class Group(Type, IntEnum):
     math = auto()
     science = auto()
     music = auto()
     place = auto()
+
     time = auto()
     network = auto()
     game = auto()
     commercial = auto()
-    other = auto()
+
+    RADIX = 16
 
 
-class Math(IntEnum):
+class Math(Type, IntEnum):
     integer = auto()
     fraction = auto()
+    floating = auto()
 
 
-class Science(IntEnum):
+class Science(Type, IntEnum):
     element = auto()
     unit = auto()
 
 
-class Music(IntEnum):
+class Music(Type, IntEnum):
     rhythm = auto()
     melody = auto()
 
 
-class Place(IntEnum):
+class Place(Type, IntEnum):
     lat_long = auto()
 
 
-class Network(IntEnum):
+class Time(Type, IntEnum):
+    instant = auto()
+
+
+class Network(Type, IntEnum):
     ip_address = auto()
     sem_ver = auto()
     uuid = auto()
 
 
-class Game(IntEnum):
+class Game(Type, IntEnum):
     backgammon = auto()
     cards = auto()
     chess = auto()
     go = auto()
 
 
-class Commercial(IntEnum):
+class Commercial(Type, IntEnum):
     isbn = auto()
     upc = auto()
+
+
+def number_to_remainder_and_type(n, strict=False):
+    d, group = Group.divmod(n, strict)
+    subtype = globals()[group.name.capitalize()]
+    return subtype.divmod(d, strict)
