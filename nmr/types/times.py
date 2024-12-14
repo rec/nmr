95f5996e00fb8d08faecc7ff376591ad6b406ac9
Tom Ritchford <tom@swirly.com>
2024-12-14 15:16:15 +0100

    Fix typing in times.py

--- nmr/types/times.py
+++ nmr/types/times.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 import dataclasses as dc
 from datetime import datetime
 from enum import IntEnum, auto
+from typing import cast
+
+MICROSECOND_TO_YOCTOSECOND: int = 10 ** (24 - 6)
 
 
 class Interval(IntEnum):
@@ -85,8 +90,8 @@ SECONDS = {
 @dc.dataclass
 class Time:
     # A location in time
-    interval: Interval = datacls.field(default=Interval.SECOND)
-    time: datetime = datacls.field(datetime.now)
+    interval: Interval = dc.field(default=Interval.SECOND)
+    time: datetime = dc.field(default_factory=datetime.now)
 
     # datetime cannot represent all of time, so add optional offsets
     yoctoseconds: int = 0
@@ -97,38 +102,35 @@ class Time:
 
     assert INTERVAL_SCALE > len(Interval)
 
-    def whole_seconds(self):
+    def whole_seconds(self) -> int:
         return int(self.time.timestamp()) + (self.years * 3562422) // 10000
 
-    def whole_years(self):
+    def whole_years(self) -> int:
         return self.time.year - self.YEAR_ZERO + self.years
 
-    def whole_microseconds(self):
-        return 10**6 * self.whole_seconds() + self.time.microseconds
+    def whole_microseconds(self) -> int:
+        return 10**6 * self.whole_seconds() + self.time.microsecond
 
-    def whole_yoctoseconds(self):
-        yoctoseconds = self.whole_microseconds() * 10 ** (24 - 6)
+    def whole_yoctoseconds(self) -> int:
+        yoctoseconds = self.whole_microseconds() * MICROSECOND_TO_YOCTOSECOND
         return yoctoseconds + self.yoctoseconds
 
     def _to_count(self) -> int:
-        div = YEARS.get(self.interval)
-        if div:
+        if div := YEARS.get(self.interval, 0):
             return self.whole_years() // div
 
-        div = SECONDS.get(self.interval)
-        if div:
+        if div := SECONDS.get(self.interval, 0):
             return self.whole_seconds() // div
 
-        div = MICROSECONDS.get(self.interval)
-        if div:
+        if div := MICROSECONDS.get(self.interval, 0):
             return self.whole_microseconds() // div
 
-        div = YOCTOSECONDS.get(self.interval)
-        if div:
-            return self.whole_yoctoseconds() // div
+        if div := YOCTOSECONDS.get(self.interval, 0):
+            # TODO(rec): why is only this cast needed?
+            return cast(int, self.whole_yoctoseconds() // div)
 
         assert self.interval == Interval.MONTH
         return 12 * self.whole_years() + self.time.month
 
-    def __call__(self):
+    def __call__(self) -> int:
         return self.interval.value + self.INTERVAL_SCALE * self._to_count()
