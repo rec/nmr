24d9acea1014d44c28e9e5f7667f8c7d0ca56da9
Tom Ritchford <tom@swirly.com>
2024-12-14 17:13:29 +0100

    Fix typing in hex.py

--- nmr/types/hex.py
+++ nmr/types/hex.py
@@ -1,13 +1,16 @@
+from __future__ import annotations
+
 from ..nameable_type import NameableType
 
 
 class Hex(NameableType):
     @staticmethod
-    def type_to_int(s: str):
+    def type_to_int(s: str) -> int | None:
         s = s.lower()
         if s.startswith("0x"):
             return int(s[2:], 16)
         if s.startswith("-0x"):
             return -int(s[2:], 16)
+        return None
 
-    int_to_type = staticmethod(hex)
+    int_to_type = staticmethod(hex)  # type: ignore[assignment]
