7195ea35aacbc231d487f9ab25855c13b9d3505d
Tom Ritchford <tom@swirly.com>
2025-01-19 19:27:19 +0100

    Simplify sem_ver.py a little

--- nmr/types/sem_ver.py
+++ nmr/types/sem_ver.py
@@ -10,12 +10,12 @@ packer = Packer(3)
 class Semver(NameableType[Version]):
     category = Computer.SEMVER
 
-    @classmethod
-    def index_to_type(cls, i: int) -> Version:
+    @staticmethod
+    def index_to_type(i: int) -> Version:
         return Version(*packer.unpack(i))
 
-    @classmethod
-    def str_to_type(cls, s: str) -> Version:
+    @staticmethod
+    def str_to_type(s: str) -> Version:
         if s.startswith("v"):
             return Version.parse(s[1:])
         raise ValueError("semantic versions must start with v")
